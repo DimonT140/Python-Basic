2ae8a52809748edcea9581443232479e80456d66
DimonT140 <vopros.140@gmail.com>
2023-03-19 14:28:16 +0200

    6.2

--- Homeworks/Homework 6/Homework 6.1.py
+++ Homeworks/Homework 6/Homework 6.1.py
@@ -3,13 +3,13 @@ import math
 
 # функція конвертує градуси в радіани
 def convert_degrees_to_radians(degree: float):
-    radian = degree * math.pi / 180  # конвертуємо градуси в радіани
+    radian = degree * math.pi / 180   # конвертуємо градуси в радіани
     rounded_radians = round(radian, 5)  # округлюємо результат до 5 знаків після коми
     return rounded_radians
 
 
 if __name__ == '__main__':
-    degrees = float(input("Введіть значення кута в градусах: "))  # запитуємо користувача про градуси
+    degrees = float(input("Введіть значення кута в градусах: "))   # запитуємо користувача про градуси
     radians = convert_degrees_to_radians(degrees)  # викликаємо функцію для конвертації
 
     # виводимо результат
