31de5f8f9a83230b0c3c73e522e732f5216b42bc
DimonT140 <vopros.140@gmail.com>
2023-03-30 17:58:01 +0300

    4

--- Homeworks/Homework 9/Homework 9.py
+++ Homeworks/Homework 9/Homework 9.py
@@ -2,7 +2,7 @@ from transformer import transform_lines
 
 
 if __name__ == '__main__':
-    file_path = 'file.txt'
+    file_path = 'notes.txt'
     transformed_lines = transform_lines(file_path)
     # Виводимо список рядків на екран
     print(transformed_lines)
