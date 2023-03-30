91a3bb87bc7dc376411111fd88cd0316fada9d1c
DimonT140 <vopros.140@gmail.com>
2023-03-30 18:31:18 +0300

    5

--- Homeworks/Homework 9/Homework 9.py
+++ Homeworks/Homework 9/Homework 9.py
@@ -3,6 +3,10 @@ from transformer import transform_lines
 
 if __name__ == '__main__':
     file_path = 'notes.txt'
-    transformed_lines = transform_lines(file_path)
-    # Виводимо список рядків на екран
-    print(transformed_lines)
+    while True:
+        transformed_lines = transform_lines(file_path)
+        # Виводимо список рядків на екран
+        print(transformed_lines)
+        choice = input("Вийти? (так/ні) ")
+        if choice.lower() == 'так':
+            break
