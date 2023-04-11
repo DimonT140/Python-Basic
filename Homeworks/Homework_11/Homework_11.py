94e1e3d1dd6a0ac91f54ce5a6888b5aee5e890db
DimonT140 <vopros.140@gmail.com>
2023-04-12 00:53:50 +0300

    02

--- Homeworks/Homework_11/Homework_11.py
+++ Homeworks/Homework_11/Homework_11.py
@@ -1,59 +1,62 @@
 import csv
 
 
-def main():
-    file_path = 'tech_inventory.csv'
-    brand_index = {}
-    category_index = {}
-    data = []
-    with open(file_path, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            category = row['category']
-            brand = row['brand']
-            data.append(row)
-            if brand not in brand_index:
-                brand_index[brand] = 1
-            else:
-                brand_index[brand] += 1
-            if category not in category_index:
-                category_index[category] = 1
-            else:
-                category_index[category] += 1
+def count_items(data, key):
+    count_index = {}
+    for row in data:
+        item = row[key]
+        if item not in count_index:
+            count_index[item] = 1
+        else:
+            count_index[item] += 1
+    for i, (item, count) in enumerate(count_index.items(), start=1):
+        if key == 'brand':
+            print(f'Від бренду "{item}" є {count} товарів')
+        else:
+            print(f'Серед категорії "{item}" є {count} товарів')
+        if i % 8 == 0:
+            input("\nНатисніть enter, щоб побачити продовження")
+            print()
 
-    for brand, count in brand_index.items():
-        print(f'Від бренду "{brand}" є {count} товарів')
-    print()
-    for category, count in category_index.items():
-        print(f'Серед категорії "{category}" є {count} товарів')
-    print()
 
-    brand = input("По якому бренду вивести повну інформацію? ")
+def get_items(data, key):
+    value = input(f"\nПо якому/якій {key} вивести повну інформацію? \nабо натисніть enter, щоб пропустити \n")
+    count = 0
     for item in data:
-        if item['brand'] == brand:
+        if item[key] == value:
             print(item)
-    print()
-    category = input("По якій категорії вивести повну інформацію? ")
-    for item in data:
-        if item['category'] == category:
-            print(item)
-    print()
-    while True:
-        category = input("введіть категорію або вихід): ")
-        if category == 'вихід':
-            break
-        if category not in category_index:
-            print(f'Категорії "{category}" не існує \n')
-            continue
+            count += 1
+            if count % 8 == 0:
+                input("\nНатисніть enter, щоб побачити продовження\n")
+
+
+def show_brand_distribution(data):
+    categories = set(item['category'] for item in data)
+    for category in categories:
         brands = {}
         for item in data:
             if item['category'] == category:
-                brand = item['brand']
-                if brand not in brands:
-                    brands[brand] = 1
+                if item['brand'] in brands:
+                    brands[item['brand']] += 1
                 else:
-                    brands[brand] += 1
-        print(f'У категорії "{category}" представлені товари таких брендів: {brands} \n')
+                    brands[item['brand']] = 1
+        print(f"У категорії \"{category}\" представлені товари таких брендів: {brands}")
+
+
+def main():
+    file_path = 'tech_inventory.csv'
+    data = []
+
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+
+    count_items(data, 'brand')
+    count_items(data, 'category')
+    get_items(data, 'brand')
+    get_items(data, 'category')
+    show_brand_distribution(data)
 
 
 if __name__ == '__main__':
